9b4fa9ed074bc841137a8472efb4b1ccdf37d88e
Will Palmer <wop3000@hotmail.co.uk>
2025-02-26 09:42:56 +0000

    added env variable loading

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from fastapi import FastAPI, HTTPException
 import httpx
 from fastapi.middleware.cors import CORSMiddleware
 from models import ActivityRequest, RateCard
+from dotenv import load_dotenv
 import os
 
+load_dotenv()
 app = FastAPI()
 
 origins = [
@@ -25,10 +27,9 @@ app.add_middleware(
 )
 
 # Get backend URL from environment variable or use default
-BACKEND_URL = os.getenv("BACKEND_URL", "http://127.0.0.1:8000")
+BACKEND_URL = os.getenv("BACKEND_URL")
 
 http_client = httpx.AsyncClient()
-
 
 @app.post("/activities")
 async def activities(initial_parameters: ActivityRequest):
@@ -36,7 +37,7 @@ async def activities(initial_parameters: ActivityRequest):
         response = await http_client.post(
             f"{BACKEND_URL}/activities", json=initial_parameters.model_dump()
         )
-        response.raise_for_status()
+        print(response)
         return response.json()
     except httpx.HTTPStatusError as e:
         raise HTTPException(
