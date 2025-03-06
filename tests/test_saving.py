b5ea1c64a677866548a2059b9237a6f7da74aa63
qczeck <maciej.kuczek@icloud.com>
2025-03-06 13:18:02 +0000

    fix: linting

--- tests/test_saving.py
+++ tests/test_saving.py
@@ -1,6 +1,7 @@
 from utils.utils import create_trip_data
 from _datetime import datetime
 
+
 def test_create_trip_data():
     trip = create_trip_data("London", ["Afternoon", "Evening"], "Couple")
 
